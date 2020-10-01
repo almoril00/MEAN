ca91b85e51b24bacfed545fd71f72c6e5bbb11db
lrag <a@b.c>
2020-10-01 19:54:51 +0200

    Jesusito de mi vida, que me quede como estoy

---
+++ 02_NodeJS/04_ReST/negocioSeries.js
@@ -0,0 +1,17 @@
+
+//El objeto exports es implícito
+//Le asignaremos como propiedades tolo lo que queramos que sea visible desde otros 
+//ficheros que incluyan a este
+
+
+
+
+
+
+exports.listarSeries = function(){
+
+}
+
+exports.buscarSeriePorId = function(){
+
+}
