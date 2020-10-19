2413891cda21c0dd4a9e601a372414fdd5f2cfd3
lrag <a@b.c>
2020-10-19 20:21:24 +0200

    Ay mama con el angular este

--- 04_Angular/Ej02-Componentes/src/app/app.module.ts
+++ 04_Angular/Ej02-Componentes/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PieComponent } from './componentes/pie/pie.component';
+import { DiscosComponent } from './componentes/discos/discos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    DiscosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 04_Angular/Ej02-Componentes/src/app/entidades/disco.ts
@@ -0,0 +1,10 @@
+
+export class Disco {
+
+    public constructor(public titulo : string = null,
+                       public autor  : string = null,
+                       public year   : number = null,
+                       public genero : string = null){
+    }
+
+}
