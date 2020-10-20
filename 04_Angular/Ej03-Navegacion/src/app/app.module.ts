09864f5e3c4873afe77cbd9ec435843a926be214
lrag <a@b.c>
2020-10-20 17:29:25 +0200

    Si es simple page application que dices tu de navegar

--- 04_Angular/Ej03-Navegacion/src/app/app.module.ts
+++ 04_Angular/Ej03-Navegacion/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { Componente1Component } from './componentes/componente1/componente1.component';
+import { Componente2Component } from './componentes/componente2/componente2.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    Componente1Component,
+    Componente2Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule
