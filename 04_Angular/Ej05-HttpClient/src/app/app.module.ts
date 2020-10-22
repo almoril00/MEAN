e6e4fd9d6365a04494e596a3a3dcb4c7b374c96a
lrag <a@b.c>
2020-10-22 18:04:35 +0200

    No vaya a ser que no veamos cuarenta formas distintas de enviar peticiones ajax...

--- 04_Angular/Ej05-HttpClient/src/app/app.module.ts
+++ 04_Angular/Ej05-HttpClient/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { PieComponent } from './componentes/pie/pie.component';
+import { ListadoUsuariosComponent } from './componentes/listado-usuarios/listado-usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
     MenuComponent,
-    PieComponent
+    PieComponent,
+    ListadoUsuariosComponent
   ],
   imports: [
     BrowserModule
