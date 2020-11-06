a79ec651810fbc092e51754f46280a6837980a06
lrag <a@b.c>
2020-11-06 16:57:53 +0100

    La tienda 1.0

--- 05_MEAN/02_Tienda_1.0/Angular/src/app/app.module.ts
+++ 05_MEAN/02_Tienda_1.0/Angular/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/usuarios/login/login.component';
+import { RegistroComponent } from './componentes/usuarios/registro/registro.component';
+import { AceptacionTerminosComponent } from './componentes/usuarios/aceptacion-terminos/aceptacion-terminos.component';
+import { CabeceraComponent } from './componentes/maquetacion/cabecera/cabecera.component';
+import { PieComponent } from './componentes/maquetacion/pie/pie.component';
+import { CatalogoComponent } from './componentes/tienda/catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    AceptacionTerminosComponent,
+    CabeceraComponent,
+    PieComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule
