ec1168d3d68a36a4d9952ddcc764d7604222fa78
lrag <a@b.c>
2020-11-24 18:24:01 +0100

    3XX VAMONOS

--- 05_MEAN/03_Tienda_2.0/Angular/src/app/util/configuracion.ts
+++ 05_MEAN/03_Tienda_2.0/Angular/src/app/util/configuracion.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable( { providedIn : 'root' } )
 export class Configuracion {
     
-    public url_servidor:string = "http://localhost:9000"
+    public url_servidor:string = "https://localhost:9000"
 
 }
 
